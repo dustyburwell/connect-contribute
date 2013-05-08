014798322e370257df86701f84ceb08934364d1f
Dusty Burwell <ascalonx@gmail.com>
2013-05-07 23:27:54 -0500

    Fill out specs

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+var url  = require("url");
+var path = require("path")
+
 module.exports = function(options) {
 
-  return function contribute(req, res, next) {
+  if (options.repository && options.repository.url) {
+    var repo = options.repository.url;
+    options.contributeUrl  = repo;
+    options.issuesLocation = repo.replace(/(\.git)?$/, "/issues");
+  }
 
+  return function contribute(req, res, next) {
+    res.headers["X-ContributeUrl"] = options.contributeUrl;
+    res.headers["X-IssuesLocation"] = options.issuesLocation;
+    next();
   }
 }
--- test/contribute_spec.js
+++ test/contribute_spec.js
@@ -1,9 +1,59 @@
+var spies  = require("spies");
+var should = require("should");
 
+var contribute = require("../");
 
-describe("contribute", function() {
-  it("should add X-ContributeUrl to response from options");
-  it("should add X-IssuesLocation to response from options");
+describe("contribute()", function() {
+  var req = {};
+  var res = { headers: {} };
 
-  it("should add X-ContributeUrl to response from repository location")
-  it("should add X-IssuesLocation to response from repository location")
+  describe("when options are explicit", function() {
+    it("should add X-ContributeUrl to response from options", function(done) {
+      var sut = contribute({ "contributeUrl": "http://example.com/contribute" })
+
+      sut(req, res, function() {
+        res.headers["X-ContributeUrl"].should.equal("http://example.com/contribute");
+        done();
+      });
+    });
+
+    it("should add X-IssuesLocation to response from options", function(done) {
+      var sut = contribute({ "issuesLocation": "http://example.com/issues" })
+
+      sut(req, res, function() {
+        res.headers["X-IssuesLocation"].should.equal("http://example.com/issues");
+        done();
+      });
+    });
+  });
+
+  it("should not set headers if the options aren't present", function(done) {
+    var sut = contribute({ })
+
+    sut(req, res, function() {
+      should.not.exist(res.headers["X-ContributeUrl"]);
+      should.not.exist(res.headers["X-IssuesLocation"]);
+      done();
+    });
+  });
+
+  describe("when package.json options are used", function() {
+    it("should add X-ContributeUrl to response from repository location", function(done) {
+      var sut = contribute({ repository: { url: "https://github.com/dustyburwell/connect-contribute.git" } })
+
+      sut(req, res, function() {
+        res.headers["X-ContributeUrl"].should.equal("https://github.com/dustyburwell/connect-contribute.git");
+        done();
+      });
+    });
+
+    it("should add X-IssuesLocation to response from repository location", function(done) {
+      var sut = contribute({ repository: { url: "https://github.com/dustyburwell/connect-contribute.git" } })
+
+      sut(req, res, function() {
+        res.headers["X-IssuesLocation"].should.equal("https://github.com/dustyburwell/connect-contribute/issues");
+        done();
+      });
+    });
+  });
 });
