10b2e200f479c344d4bb4ce6683b97264ab69a28
Dusty Burwell <ascalonx@gmail.com>
2013-05-07 23:46:38 -0500

    Add a few more edge cases

--- test/contribute_spec.js
+++ test/contribute_spec.js
@@ -7,51 +7,93 @@ describe("contribute()", function() {
   var req = {};
   var res = { headers: {} };
 
-  describe("when options are explicit", function() {
-    it("should add X-ContributeUrl to response from options", function(done) {
-      var sut = contribute({ "contributeUrl": "http://example.com/contribute" })
+  var because = function(options, callback) {
+    var sut = contribute(options);
+    sut(req, res, callback);
+  };
 
-      sut(req, res, function() {
+  describe("when no options are passed", function() {
+    var options = null;
+
+    it("should not set headers", function(done) {
+      because(options, function() {
+        should.not.exist(res.headers["X-ContributeUrl"]);
+        should.not.exist(res.headers["X-IssuesLocation"]);
+        done();
+      });
+    });
+  });
+
+  describe("when options are empty", function() {
+    var options = {};
+
+    it("should not set headers", function(done) {
+      because(options, function() {
+        should.not.exist(res.headers["X-ContributeUrl"]);
+        should.not.exist(res.headers["X-IssuesLocation"]);
+        done();
+      });
+    });
+  });
+
+  describe("when options are explicit", function() {
+    var options = {
+      contributeUrl: "http://example.com/contribute",
+      issuesLocation: "http://example.com/issues"
+    };
+
+    it("should add X-ContributeUrl to response from options", function(done) {
+      because(options, function() {
         res.headers["X-ContributeUrl"].should.equal("http://example.com/contribute");
         done();
       });
     });
 
     it("should add X-IssuesLocation to response from options", function(done) {
-      var sut = contribute({ "issuesLocation": "http://example.com/issues" })
-
-      sut(req, res, function() {
+      because(options, function() {
         res.headers["X-IssuesLocation"].should.equal("http://example.com/issues");
         done();
       });
     });
   });
 
-  it("should not set headers if the options aren't present", function(done) {
-    var sut = contribute({ })
-
-    sut(req, res, function() {
-      should.not.exist(res.headers["X-ContributeUrl"]);
-      should.not.exist(res.headers["X-IssuesLocation"]);
-      done();
-    });
-  });
-
   describe("when package.json options are used", function() {
-    it("should add X-ContributeUrl to response from repository location", function(done) {
-      var sut = contribute({ repository: { url: "https://github.com/dustyburwell/connect-contribute.git" } })
+    var options = { repository: { url: "https://github.com/dustyburwell/connect-contribute.git" } };
 
-      sut(req, res, function() {
+    it("should add X-ContributeUrl to response from repository location", function(done) {
+      because(options, function() {
         res.headers["X-ContributeUrl"].should.equal("https://github.com/dustyburwell/connect-contribute.git");
         done();
       });
     });
 
     it("should add X-IssuesLocation to response from repository location", function(done) {
-      var sut = contribute({ repository: { url: "https://github.com/dustyburwell/connect-contribute.git" } })
-
-      sut(req, res, function() {
+      because(options, function() {
         res.headers["X-IssuesLocation"].should.equal("https://github.com/dustyburwell/connect-contribute/issues");
+        done();
+      });
+    });
+  });
+
+  describe("when package.json options are used and overrides are specified", function() {
+    var options = { 
+      contributeUrl: "http://example.com/contribute",
+      issuesLocation: "http://example.com/issues",
+      repository: { 
+        url: "https://github.com/dustyburwell/connect-contribute.git" 
+      } 
+    };
+
+    it("should add X-ContributeUrl to response from options", function(done) {
+      because(options, function() {
+        res.headers["X-ContributeUrl"].should.equal("http://example.com/contribute");
+        done();
+      });
+    });
+
+    it("should add X-IssuesLocation to response from options", function(done) {
+      because(options, function() {
+        res.headers["X-IssuesLocation"].should.equal("http://example.com/issues");
         done();
       });
     });
