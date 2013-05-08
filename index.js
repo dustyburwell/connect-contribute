36ac8d5263473d71830facfbd3f80fcc15ee1cb6
Dusty Burwell <ascalonx@gmail.com>
2013-05-07 21:08:00 -0500

    Add some initial specs

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = function(options) {
+
+  return function contribute(req, res, next) {
+
+  }
+}
---
+++ test/contribute_spec.js
@@ -0,0 +1,9 @@
+
+
+describe("contribute", function() {
+  it("should add X-ContributeUrl to response from options");
+  it("should add X-IssuesLocation to response from options");
+
+  it("should add X-ContributeUrl to response from repository location")
+  it("should add X-IssuesLocation to response from repository location")
+});
